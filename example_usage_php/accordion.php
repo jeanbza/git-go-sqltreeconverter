1632d0edbfdd8824bd04c1f78ed3af48091574df
Jean de Klerk <jadekler@gmail.com>
2015-05-26 07:04:38 -0600

    Initial partially working commit for backend hooked up to accordian

--- example_usage_php/accordion.php
+++ example_usage_php/accordion.php
@@ -1,12 +1,31 @@
+<?php
+
+include 'backend.php';
+
+// Let's pretend Wayne Laubscher (id = 7) is logged in
+$specific_member_id = 7;
+$specific_members_sql = '
+  SELECT id, member_type_id, lft, rght, first_name, last_name
+  FROM tree_example.members
+  WHERE lft >= (SELECT lft FROM tree_example.members WHERE id = ' . $specific_member_id . ')
+  AND rght <= (SELECT rght FROM tree_example.members WHERE id = ' . $specific_member_id . ')
+  ORDER BY lft
+';
+$specific_members_with_lefts_and_rights = getNodesFromDatabase($specific_members_sql);
+$specific_members_with_children_and_parents = unserializeFromDatabase($specific_members_with_lefts_and_rights);
+$specific_members_array = $specific_members_with_children_and_parents->to_array(4);
+
+?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>jQuery UI Accordion - Default functionality</title>
     <script src="static/js/external/jquery/jquery-2.0.3.min.js"></script>
     <script>
-        $(document).ready(function() {
-            $('.accordion .head').click(function() {
+        $(document).ready(function () {
+            $('.accordion .head').click(function () {
                 $(this).next().toggle('slow');
                 return false;
             }).next().hide();
@@ -30,52 +49,18 @@
 <body>
 
 <div class="accordion">
-    <h3 class="head">Section 1</h3>
-    <div class="content">
-        <p>
-            Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
-            ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
-            amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
-            odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
-        </p>
-    </div>
-    <h3 class="head">Section 2</h3>
-    <div class="content">
-        <p>
-            Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
-            purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
-            velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
-            suscipit faucibus urna.
-        </p>
-    </div>
-    <h3 class="head">Section 3</h3>
-    <div class="content">
-        <p>
-            Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
-            Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
-            ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
-            lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
-        </p>
-        <ul>
-            <li>List item one</li>
-            <li>List item two</li>
-            <li>List item three</li>
-        </ul>
-    </div>
-    <h3 class="head">Section 4</h3>
-    <div class="content">
-        <p>
-            Cras dictum. Pellentesque habitant morbi tristique senectus et netus
-            et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
-            mauris vel est.
-        </p>
-        <p>
-            Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-            inceptos himenaeos.
-        </p>
-    </div>
+    <?php
+    foreach ($specific_members_array as $level => $members_at_level) {
+        echo '<h3 class="head">Level ' . $level . '</h3>';
+        echo '<div class="content">';
+
+        foreach ($members_at_level as $member) {
+            echo '<div>' . $member->name . '</div>';
+        }
+
+        echo '</div>';
+    }
+    ?>
 </div>
 
 
---
+++ example_usage_php/backend.php
@@ -0,0 +1,155 @@
+<?php
+
+// Note: this should be an api that your javascript consumes with AJAX
+
+class MemberWithLeftsAndRights {
+    public $id;
+    public $member_type;
+    public $name;
+    public $lft;
+    public $rgt;
+
+    function __construct($id, $member_type, $lft, $rgt, $first_name, $last_name) {
+        $this->id = $id;
+        $this->member_type = $member_type;
+        $this->lft = $lft;
+        $this->rgt = $rgt;
+        $this->name = $first_name . " " . $last_name;
+    }
+}
+
+class MemberWithChildrenAndParents {
+    public $id;
+    public $member_type;
+    public $name;
+
+    public $lft;
+    public $rgt;
+
+    public $parent;
+    public $children;
+
+    function __construct($id, $member_type, $name, $lft, $rgt, &$parent, &$children) {
+        $this->id = $id;
+        $this->member_type = $member_type;
+        $this->name = $name;
+
+        $this->lft = $lft;
+        $this->rgt = $rgt;
+
+        $this->parent = $parent;
+        $this->children = $children;
+    }
+
+    function to_json() {
+        $json = '{';
+
+        $json .= '"id":' . $this->id . ',"member_type":' . $this->member_type . ',"name":"' . $this->name . '","lft":' . $this->lft . ',"rgt":' . $this->rgt . ',"children":[';
+
+        foreach ($this->children as $index => $child) {
+            if ($index != 0) {
+                $json .= ',';
+            }
+
+            $json .= $child->to_json();
+        }
+
+        return $json . ']}';
+    }
+
+    function to_array($levels = 0) {
+        $members_at_each_level = array();
+        $members_at_this_level = array($this); // start by processing root
+        $members_at_next_level = array();
+
+        // Levels+1 because otherwise we'd never get to the root element
+        for ($i = 0; $i < $levels + 1; $i++) {
+            array_push($members_at_each_level, array());
+
+            foreach ($members_at_this_level as $member) {
+                array_push($members_at_each_level[$i], $member);
+                array_merge($members_at_next_level, $member->children);
+            }
+
+            $members_at_this_level = $members_at_next_level;
+            $members_at_next_level = array();
+        }
+
+        return $members_at_each_level;
+    }
+}
+
+function getNodesFromDatabase($sql) {
+    $members = array();
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    $result = $conn->query($sql);
+
+    while ($row = $result->fetch_assoc()) {
+        array_push($members, new MemberWithLeftsAndRights($row["id"], $row["member_type_id"], $row["lft"], $row["rght"], $row["first_name"], $row["last_name"]));
+    }
+
+    $conn->close();
+
+    return $members;
+}
+
+// Convert left/right objects to tree with parent+child relationships
+// Note: data ordered by left (in sql: ORDER BY lft) is very important for this to work
+// Note: this runs in nlogn, which is far better than the n^2 if we were using parent_id in database
+function unserializeFromDatabase($membersWithLeftsAndRights) {
+    $root = null;
+    $node = null;
+
+    $foo = 0; // REMOVE THIS
+
+    while (0 < sizeof($membersWithLeftsAndRights) && $foo < 100) {
+        $foo++; // REMOVE THIS
+
+        $member = $membersWithLeftsAndRights[0];
+        $membersWithLeftsAndRights = array_slice($membersWithLeftsAndRights, 1, sizeof($membersWithLeftsAndRights));
+
+        if ($root == null) {
+            $root = new MemberWithChildrenAndParents($member->id, $member->member_type, $member->name, $member->lft, $member->rgt, $tmp = null, $array = array());
+            $node = $root;
+        } else {
+            // REMOVE THIS
+            if ($node->rgt == null) {
+                return;
+            }
+
+            // REMOVE NULLCHECK
+            while ($node != null && $member->lft > $node->rgt && $foo < 100) {
+                $foo++; // REMOVE THIS
+                $node = $node->parent;
+            }
+
+            array_push($node->children, new MemberWithChildrenAndParents($member->id, $member->member_type, $member->name, $member->lft, $member->rgt, $node, $array = array()));
+
+            if ($node->lft != $node->rgt - 1) {
+                $node = $node->children[sizeof($node->children) - 1];
+            }
+        }
+    }
+
+    return $root;
+}
+
+function limitChildrenToFourDepth(&$member, $depth = 0) {
+    if ($depth == 3) {
+        // throw away all children below depth 3
+        $member->children = array();
+    } else {
+        for ($i = 0; $i < sizeof($member->children); $i++) {
+            limitChildrenToFourDepth($member->children[$i], $depth + 1);
+        }
+    }
+}
+
+?>
